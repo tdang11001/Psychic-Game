4d377678270d4ba7e13fbe57c70e3e4982833a9d
Tony Dang <tdang11001@gmail.com>
2018-12-06 03:18:09 -0500

    finally done

--- assets/js/guess.js
+++ assets/js/guess.js
@@ -20,95 +20,41 @@ var userGuessCharacter;
 // computer random character
 var computerRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-
-
+// grab the p id "win" and stored it in the winTarget variable
 var winTarget = document.getElementById("wins");
 
-// document.getElementById('wins').innerHTML = win;
-// document.getElementById('losses').innerHTML = win;
-
-
-
-
+// what to do when the key is released
 $(document).keyup(function(event) {
-    userGuessCharacter = event.key;
-    console.log(userGuessCharacter);
+    userGuessCharacter = event.key; // stored the key pressed in a variable, userGuessCharacter
+    // console.log(userGuessCharacter);
 
 
-    userGuessArray.push(userGuessCharacter);
-    $("#guessArraySpan").text(userGuessArray, + " , ");
+    userGuessArray.push(userGuessCharacter); // stored each chracter press inside of the array
+    $("#guessArraySpan").text(userGuessArray); // print each stored character array to the page
     
-   
+    // what to do when the user guess matches the computer random letter
     if (userGuessCharacter === computerRandomLetter) {
-        win += 1;
-        $("#winsSpan").text(win);
-    }
-    else {
-        numberOfTries -= 1;
-        $("#guessLeftSpan").text(numberOfTries);
-    }
-
-    if (numberOfTries == 0) {
-        loss += 1;
-        alert('Game over. The computer character is ' + computerRandomLetter);
-        $("#lossesSpan").text(loss);
-        numberOfTries = 10;
-        $("#guessLeftSpan").text(numberOfTries);
-        userGuessArray = [];
+        win += 1; // increment win by 1
+        $("#winsSpan").text(win); // display number of win to the page
+        alert('You win! The computer random character is ' + computerRandomLetter); // alert the computer random letter
+        computerRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)]; // generate random letter
+        $("#guessLeftSpan").text(numberOfTries); // display the number of tries to the page
+        userGuessArray = []; // reset to empty array
+        numberOfTries = 10; // reset number of tries
 
     }
+    else { // if the guess does not match
+        numberOfTries -= 1; // decrement the number of tries by 1
+        $("#guessLeftSpan").text(numberOfTries); // display number of tries to the page
 
-    i
-
-
-
-   
-
-
-
-
-
+        if (numberOfTries == 0) { // if the number of tries equal to 0
+            loss += 1; // increment number of loss by 1
+            alert('You lose! Game over! The computer random character is ' + computerRandomLetter); // alert the computer random letter
+            $("#lossesSpan").text(loss); // display the number of loss to the page
+            numberOfTries = 10; // reset number of tries
+            $("#guessLeftSpan").text(numberOfTries); // display number of guess left to the page
+            userGuessArray = []; // reset the user guess array
+    
+        }
+    }
 }); 
-
-
-
-
-
-
-
-// let lossTarget = document.getElementById("losses");
-
-// let numberOfTriesTarget = document.getElementById("guessLeft");
-
-// winTarget.innerHTML = "Hi";
-
-// let userInput = 
-
-
-
-
-// if ( computerRandomLetter === userGuessArray) {
-//     win++;
-// }
-// else {
-//     numberOfTries--;
-// }
-
-// if (numberOfTries == 0) {
-//     alert("Game over! Computer's letter is " + computerRandomLetter);
-//     loss--;
-// }
-
-// function resetGame() {
-//     numberOfTries = 10;
-//     userGuessArray[];
-// }
-
-// function 
-
-
-
-
-
-
-
